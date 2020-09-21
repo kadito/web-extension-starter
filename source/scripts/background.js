127fd4f20b3d2fc6a46a0cc8b3b17d7bed77a00c
Ricardo Bernardes <ricardo.bernardes@webtales.fr>
2020-09-21 18:10:05 +0100

    extension working

--- source/scripts/background.js
+++ source/scripts/background.js
@@ -1,26 +1,34 @@
 import 'emoji-log';
 import browser from 'webextension-polyfill';
 
+let JWT = null;
+browser.storage.local.get('token').then(({token}) => {
+  JWT = token;
+});
 
 browser.webRequest.onBeforeSendHeaders.addListener(
-  async function(details) {
-    console.log(details)
-    let jwt;
-    await browser.storage.local.get('token').then(({token}) => {
-      jwt = token;
-    })
+  (details) => {
+    // Only if we have a Token on extension storage
+    if (JWT) {
+      // Remove Authorization Header
+      const index = details.requestHeaders.findIndex(
+        (req) => req.name.toLowerCase() === 'authorization'
+      );
 
-    // Possible to remove -> CRM don't have authorization Header
-    let index = details.requestHeaders.findIndex(req => req.name.toLowerCase() === 'Authorization')
-    details.requestHeaders.splice(index, 1);
+      if (index !== -1) {
+        details.requestHeaders.splice(index, 1);
+      }
 
-    details.requestHeaders.push({
-      name: "Authorization",
-      value: `teste 123`
-    })
-    
+      details.requestHeaders.push({
+        name: 'authorization',
+        value: `Bearer ${JWT}`,
+      });
+    }
+
     return {requestHeaders: details.requestHeaders};
   },
-  {urls: ["<all_urls>"]},
-  ["blocking", "requestHeaders"]
+  {
+    urls: ['https://uh09ot7ael.execute-api.eu-west-1.amazonaws.com/Prod/crm'],
+  },
+  ['blocking', 'requestHeaders']
 );
--- source/scripts/popup.js
+++ source/scripts/popup.js
@@ -2,18 +2,20 @@ import 'emoji-log';
 import browser from 'webextension-polyfill';
 import fetch from 'node-fetch';
 
-document.addEventListener('DOMContentLoaded', async () => {
-  console.log("START")
-  let jwt;
-  await browser.storage.local.get('token').then(({token}) => {
-    jwt = token;
-  })
+let JWT = null;
+browser.storage.local.get('token').then(({token}) => {
+  JWT = token;
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  // console.log('START');
 
   // Already Login
-  if(jwt){
-    document.getElementById("login-form").classList.add('hidden')
+  if (JWT) {
+    document.getElementById('login-form').classList.add('hidden');
 
-    document.getElementById("login-success").classList.remove('hidden')
+    document.getElementById('login-success').classList.remove('hidden');
+    document.getElementById('login-success').classList.add('flex');
   }
 
   // Login method!
@@ -23,30 +25,31 @@ document.addEventListener('DOMContentLoaded', async () => {
     form.username = document.getElementById('username').value;
     form.password = document.getElementById('password').value;
 
-    if(form.username && form.password){
-      const query = JSON.stringify({ query: `query { authorizationToken(username:"${form.username}", password:"${form.password}") }` });
+    if (form.username && form.password) {
+      const query = JSON.stringify({
+        query: `query { authorizationToken(username:"${form.username}", password:"${form.password}") }`,
+      });
 
-      fetch("http://localhost:8080/", {
+      fetch('http://localhost:8080/', {
         method: 'POST',
         body: query,
         headers: {
-          'content-Type': 'application/json'
-        }
-      }).then(async response => {
+          'content-Type': 'application/json',
+        },
+      }).then(async (response) => {
         const json = await response.json();
-        if(json.data.authorizationToken){
-          document.getElementById("login-form").classList.add('hidden')
-  
-          browser.storage.local.set({token: json.data.authorizationToken.token })
-  
-  
-          browser.storage.local.get('token').then(({token}) => {
-            console.log("token -> ", token)
-          })
-        }
-        // Handle wrong credentials 
+        if (json.data.authorizationToken) {
+          document.getElementById('login-form').classList.add('hidden');
 
-      })
+          document.getElementById('login-success').classList.remove('hidden');
+          document.getElementById('login-success').classList.add('flex');
+
+          browser.storage.local.set({
+            token: json.data.authorizationToken.token,
+          });
+        }
+        // Handle wrong credentials
+      });
     }
   });
 });
